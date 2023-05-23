85a2f45d81a377510b198539bdb9d691f45e19f9
RAVALOHERY <ravaloheryh@gamil.com>
2023-05-23 06:15:17 +0300

    23/05/23 FIRST

--- src/COMPONENT/pages/Home.js
+++ src/COMPONENT/pages/Home.js
@@ -2,8 +2,10 @@ export default function Home(){
     return (
         <div className="home">
             <div className="boutton">
-                <a id="con" href="" >Connexion</a>
-                <a id="ins"  href="" >Inscription</a>
+                <div>
+                     <a id="con" href="" >Connexion</a>
+                     <a id="ins"  href="" >Inscription</a>
+                </div>
             </div>
         </div>
     )
